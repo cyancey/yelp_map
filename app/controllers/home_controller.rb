605957592cb7a9b79ce8c212cc2ce45803610ce3
Chris Yancey <cyancey1@gmail.com>
2014-11-28 20:54:14 -1000

    if a restaurant is showing two open times for the day (i.e. lunch and dinner service), split the times onto two lines

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -19,8 +19,22 @@ class HomeController < ApplicationController
     p params
     yelp_page = Nokogiri::HTML(open(params['yelp_page_url'],  "User-Agent" => ENV['USER_AGENT']))
     price_range = yelp_page.css('.price-range').children[0].text
-    todays_hours = yelp_page.css('.island.summary .biz-hours .hour-range').text
+    todays_hours = parse_todays_hours(yelp_page)
+    # todays_hours = yelp_page.css('.island.summary .biz-hours .hour-range').text
     render json: {price_range: price_range,
                   todays_hours: todays_hours}
+  end
+
+  private
+
+  def parse_todays_hours(yelp_page)
+    if yelp_page.css('.island.summary .biz-hours .hour-range span').length > 2
+      hours_text =  /(.+(am|pm))(\d.+)/.match(yelp_page.css('.island.summary .biz-hours .hour-range').text)
+      [{hours_text: hours_text[1]}, {hours_text: hours_text[3]}]
+    elsif yelp_page.css('.island.summary .biz-hours .hour-range span').length == 2
+      [{hours_text:yelp_page.css('.island.summary .biz-hours .hour-range').text}]
+    else
+      nil
+    end
   end
 end
