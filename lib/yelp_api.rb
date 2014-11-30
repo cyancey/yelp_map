9c778a69ce59dc42493b16c8570a06346f410985
Chris Yancey <cyancey1@gmail.com>
2014-11-29 20:10:42 -1000

    fixed initial search to correctly use location passed from html5 geolocation

--- lib/yelp_api.rb
+++ lib/yelp_api.rb
@@ -4,12 +4,10 @@ module YelpAPI
     location: 'San Francisco',
     term: 'restaurants',
     sort_mode: 2, #highest rated
-    latitude: 19.572417,
-    longitude: -155.966742
   }
 
   def search_by_location_name(args = {})
-    search_params = DEFAULT_SEARCH_OPTIONS.reject{|k,v| k==:latitude || k==:longitude}.merge(args)
+    search_params = DEFAULT_SEARCH_OPTIONS.merge(args)
     location = search_params.delete(:location)
     results = JSON.parse(Yelp.client.search(location, search_params).to_json)
     {region: results.fetch('region', nil),
@@ -19,15 +17,15 @@ module YelpAPI
   def search_by_lat_lng(args = {})
     search_params = DEFAULT_SEARCH_OPTIONS.reject{|k,v| k==:location}.merge(args)
     results = JSON.parse(Yelp.client.search_by_coordinates(
-      {latitude: search_params.delete(:latitude),
-       longitude: search_params.delete(:longitude)},
+      {latitude: search_params.delete('lat'),
+       longitude: search_params.delete('lng')},
        search_params).to_json)
     {region: results.fetch('region', nil),
      businesses: results.fetch('businesses', nil)}
   end
 
   def search_by_bounds(args = {})
-    search_params = DEFAULT_SEARCH_OPTIONS.reject{|k,v| k==:latitude || k==:longitude || k==:location}.merge(args)
+    search_params = DEFAULT_SEARCH_OPTIONS.reject{|k,v| k==:location}.merge(args)
     bounding_box = {sw_latitude: search_params.delete("sw_latitude"),
       sw_longitude: search_params.delete("sw_longitude"),
       ne_latitude: search_params.delete("ne_latitude"),
